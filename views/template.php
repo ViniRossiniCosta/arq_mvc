e52bde401052c55dc79b5a48ae8010ba1c36cee9
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2023-09-22 10:04:19 -0300

    salve salve, salvando

--- views/template.php
+++ views/template.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Youtube</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/template.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
